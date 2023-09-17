01edafff220535d81f36c8c0dd266c842278767e
Roman Buldygin <froooks@gmail.com>
2023-09-17 22:32:56 +0300

    New project

---
+++ src/OpenHdDriver/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenHdDriver.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ src/OpenHdDriver/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace OpenHdDriver.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
